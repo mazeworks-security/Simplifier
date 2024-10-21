2a4c4a2393ae86b5e0297f6d0b04095461cf5932
Colton1skees <Colton1skees@gmail.com>
2024-10-21 11:57:26 -0400

    Add utility for changing expression bitwidths

---
+++ Mba.Simplifier/Utility/AstRewriter.cs
@@ -0,0 +1,31 @@
+﻿using Mba.Simplifier.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mba.Simplifier.Utility
+{
+    public static class AstRewriter
+    {
+        public static AstIdx ChangeBitwidth(AstCtx ctx, AstIdx node, uint newWidth)
+        {
+            var opcode = ctx.GetOpcode(node);
+            var binop = () => ctx.Binop(opcode, ChangeBitwidth(ctx, ctx.GetOp0(node), newWidth), ChangeBitwidth(ctx,ctx.GetOp1(node), newWidth));
+
+            return opcode switch
+            {
+                AstOp.Add => binop(),
+                AstOp.Mul => binop(),
+                AstOp.And => binop(),
+                AstOp.Or => binop(),
+                AstOp.Xor => binop(),
+                AstOp.Neg => ctx.Neg(ChangeBitwidth(ctx, ctx.GetOp0(node), newWidth)),
+                AstOp.Constant => ctx.Constant(ctx.GetConstantValue(node), newWidth),
+                AstOp.Symbol => ctx.Symbol(ctx.GetSymbolName(node), (byte)newWidth),
+                _ => throw new InvalidOperationException($"Cannot change width of opcode {opcode}"),
+            };
+        }
+    }
+}
