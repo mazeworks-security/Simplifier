ee949339556032299174aa4a5d908b54911b0bb8
Colton1skees <Colton1skees@gmail.com>
2025-05-04 08:29:43 -0400

    Start adding ISLE backend

---
+++ Mba.Simplifier/DSL/IsleBackend.cs
@@ -0,0 +1,19 @@
+﻿using Mba.Common.MSiMBA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mba.Simplifier.DSL
+{
+    public static class IsleBackend
+    {
+        private static string GenerateIsleDsl(IReadOnlyList<DslRule> rules)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+
+    }
+}
