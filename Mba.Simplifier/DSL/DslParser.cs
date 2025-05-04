7faa43a78f02789e8a5e28cc56cd18003ab2befe
Colton Skees <Colton1skees@users.noreply.github.com>
2025-05-04 08:30:28 -0400

    Add parser for term rewriting DSL

---
+++ Mba.Simplifier/DSL/DslParser.cs
@@ -0,0 +1,38 @@
+﻿using Mba.Ast;
+using Mba.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mba.Simplifier.DSL
+{
+    public record DslRule(string name, AstNode before, AstNode after);
+
+    public static class DslParser
+    {
+        public static IReadOnlyList<DslRule> Parse(string fileContents)
+        {
+            var lines = fileContents.Split(Environment.NewLine);
+
+            var rules = new List<DslRule>();
+            foreach(var line in lines)
+            {
+                // Skip empty lines
+                if (line.Length == 0 || line == Environment.NewLine)
+                    continue;
+
+                var split = line.Replace(" ", "").Split(new string[] { ":", "=>" }, StringSplitOptions.RemoveEmptyEntries);
+                var name = split[0];
+                var before = AstParser.Parse(split[1], 64);
+                var after = AstParser.Parse(split[2], 64);
+
+                var rule = new DslRule(name, before, after);
+                rules.Add(rule);
+            }
+
+            return rules;
+        }
+    }
+}
