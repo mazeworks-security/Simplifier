a4cc7c15d4e170297f057d4b5dec3bb05366dfe0
Colton1skees <Colton1skees@gmail.com>
2025-05-07 10:37:57 -0400

    Delete dead linear simplifier code originating from SiMBA

--- Simplifier/Program.cs
+++ Simplifier/Program.cs
@@ -11,10 +11,10 @@ using System.ComponentModel;
 using System.Diagnostics;
 
 bool printUsage = false;
-uint bitWidth = 64;
+uint bitWidth = 32;
 bool useEqsat = false;
 bool proveEquivalence = false;
-string inputText = null;
+string inputText = "(v2 & 0x9fd64269) ^ (v8 & 0x6029bd96) ^ (v2 & v6) ^ (v6 & v8)";
 
 var printHelp = () =>
 {
