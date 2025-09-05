9dbad87cb2f5415a45e5313cea0d81c260d03818
Colton1skees <Colton1skees@gmail.com>
2025-09-05 18:59:20 -0400

    refactor knownbits interface

--- EqSat/src/known_bits.rs
+++ EqSat/src/known_bits.rs
@@ -11,6 +11,121 @@ pub struct KnownBits {
     ones: u64,
 }
 
+impl KnownBits {
+    pub fn new(width: u8, zeroes: u64, ones: u64) -> Self {
+        Self {
+            width,
+            zeroes,
+            ones,
+        }
+    }
+
+    pub fn empty(width: u8) -> Self {
+        Self {
+            width,
+            zeroes: 0,
+            ones: 0,
+        }
+    }
+
+    pub fn constant(c: u64, width: u8) -> Self {
+        let mask = u64::MAX >> (64 - width);
+        Self {
+            width,
+            zeroes: !c & mask,
+            ones: c & mask,
+        }
+    }
+
+    pub fn add(lhs: &KnownBits, rhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetAddKnownBits(lhs, rhs, &mut out);
+        }
+        out
+    }
+
+    pub fn sub(lhs: &KnownBits, rhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetSubKnownBits(lhs, rhs, &mut out);
+        }
+        out
+    }
+
+    pub fn mul(lhs: &KnownBits, rhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetMulKnownBits(lhs, rhs, &mut out);
+        }
+        out
+    }
+
+    pub fn and(lhs: &KnownBits, rhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetAndKnownBits(lhs, rhs, &mut out);
+        }
+        out
+    }
+
+    pub fn or(lhs: &KnownBits, rhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetOrKnownBits(lhs, rhs, &mut out);
+        }
+        out
+    }
+
+    pub fn xor(lhs: &KnownBits, rhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetXorKnownBits(lhs, rhs, &mut out);
+        }
+        out
+    }
+
+    pub fn neg(lhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetNegKnownBits(lhs, &mut out);
+        }
+        out
+    }
+
+    pub fn shl(lhs: &KnownBits, rhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetShlKnownBits(lhs, rhs, &mut out);
+        }
+        out
+    }
+
+    pub fn lshr(lhs: &KnownBits, rhs: &KnownBits) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetLshrKnownBits(lhs, rhs, &mut out);
+        }
+        out
+    }
+
+    pub fn zext(lhs: &KnownBits, width: u32) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetZextKnownBits(lhs, width, &mut out);
+        }
+        out
+    }
+
+    pub fn trunc(lhs: &KnownBits, width: u32) -> KnownBits {
+        let mut out = KnownBits::default();
+        unsafe {
+            GetTruncKnownBits(lhs, width, &mut out);
+        }
+        out
+    }
+}
+
 #[link(name = "Mba.FFI", kind = "raw-dylib")]
 extern "C" {
     fn GetAddKnownBits(
