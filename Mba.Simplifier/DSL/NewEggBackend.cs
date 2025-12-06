b570b01ca92b95afc3476bd437743a36862a1abb
Colton1skees <Colton1skees@gmail.com>
2025-12-06 13:44:26 -0500

    inline dsl calls

--- Mba.Simplifier/DSL/NewEggBackend.cs
+++ Mba.Simplifier/DSL/NewEggBackend.cs
@@ -33,43 +33,46 @@ namespace Mba.Simplifier.DSL
 
         private void InlineFunctions()
         {
+            // Recursively inline all functions called by other DSL functions
             foreach(var dslFunction in dslFunctions)
             {
                 // Skip bodyless functions.
                 if (dslFunction.IsBuiltin)
                     continue;
 
-                Func<AstNode, AstNode> clonePred = (AstNode node) =>
-                {
-                    // Skip if this is not an intrinsic call.
-                    if (node is not IntrinsicCallNode intrinsicCall)
-                        return null;
-
-                    // Skip if the call cannot be inlined
-                    var callTarget = nameToFunc[intrinsicCall.Name];
-                    if (callTarget.IsBuiltin)
-                    {
-                        return new IntrinsicCallNode(intrinsicCall.Name, callTarget.ReturnType.Width, intrinsicCall.Children.Select(x => AstCloner.Clone(x, clonePred)).ToList());
-                    }
-
-                    // Otherwise the call can be inlined.
-                    Dictionary<string, AstNode> varToReplacement = new();
-                    for(int i = 0; i < callTarget.Arguments.Count; i++)
-                    {
-                        var arg = callTarget.Arguments[i];
-                        varToReplacement[arg.Name] = intrinsicCall.Children[i];
-                    }
-
-                    var inlined = AstCloner.ReplaceVars(callTarget.Body, varToReplacement);
-
-                    return inlined;
-                };
-
-                var newBody = AstCloner.Clone(dslFunction.Body, clonePred);
+                var newBody = AstCloner.Clone(dslFunction.Body, CloneIntrinsic);
                 var s = newBody.ToString();
                 Debugger.Break();
-
             }
         }
+
+        private AstNode CloneIntrinsic(AstNode node)
+        {
+            // Skip if this is not an intrinsic call.
+            if (node is not IntrinsicCallNode intrinsicCall)
+                return null;
+
+            // Skip if the call cannot be inlined
+            var clonedChildren = intrinsicCall.Children.Select(x => AstCloner.Clone(x, CloneIntrinsic)).ToList();
+            var callTarget = nameToFunc[intrinsicCall.Name];
+            if (callTarget.IsBuiltin)
+            {
+                return new IntrinsicCallNode(intrinsicCall.Name, callTarget.ReturnType.Width, clonedChildren.ToList());
+            }
+
+            // Otherwise the call can be inlined.
+            Dictionary<string, AstNode> varToReplacement = new();
+            for (int i = 0; i < callTarget.Arguments.Count; i++)
+            {
+                var arg = callTarget.Arguments[i];
+                varToReplacement[arg.Name] = clonedChildren[i];
+            }
+
+            var inlined = AstCloner.ReplaceVars(callTarget.Body, varToReplacement);
+
+            return inlined;
+        }
+
+
     }
 }
