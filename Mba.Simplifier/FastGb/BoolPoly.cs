6eb685fdac86726c94a064f71ef7c0baafa05ebe
Colton1skees <Colton1skees@gmail.com>
2025-12-30 07:51:33 -0500

    kinda almost works

--- Mba.Simplifier/FastGb/BoolPoly.cs
+++ Mba.Simplifier/FastGb/BoolPoly.cs
@@ -15,6 +15,11 @@ namespace Mba.Simplifier.FastGb
         static abstract int NumBits { get; }
 
         static abstract int NumWords { get; }
+
+        ulong GetWord(int index);
+
+        bool IsZero();
+
     }
 
     public struct U4 : IVector<Vector64<ulong>, U4>
@@ -66,13 +71,25 @@ namespace Mba.Simplifier.FastGb
         public static int NumVars => T.NumVars;
 
         public static int NumBits => T.NumBits;
+
+        public int VarMask;
+
+        public Monomial(int varMask)
+        {
+            VarMask = varMask;
+        }
     }
 
 
     // Dense truth table in algebraic normal form
     public class BoolPoly<T> where T : IVector<ulong, T>, IBitwiseOperators<T, T, T>
     {
-        public IVector<ulong, T> Value;
+        public T value;
+
+        public IEnumerable<Monomial<T>> Monomials => GetMonomials();
+
+        // Only exists for the debugger view, because I hate expanding enumerators
+        public List<Monomial<T>> MList => GetMonomials().ToList();
 
         public BoolPoly()
         {
@@ -81,20 +98,67 @@ namespace Mba.Simplifier.FastGb
 
         public BoolPoly(T value)
         {
-            Value = value;
+            this.value = value;
         }
 
-        private void Tzcnt()
+        public Monomial<T> Lm()
         {
-          
+            var index = LeadingZeroCount();
+            return index == T.NumBits ? new(0) : new(index);
         }
 
-        public static BoolPoly<T> operator +(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { Value = left.Value ^ right.Value };
-        public static BoolPoly<T> operator *(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { Value = left.Value & right.Value };
-        public static BoolPoly<T> operator &(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { Value = left.Value & right.Value };
-        public static BoolPoly<T> operator |(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { Value = left.Value | right.Value };
-        public static BoolPoly<T> operator ^(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { Value = left.Value ^ right.Value };
-        public static BoolPoly<T> operator ~(BoolPoly<T> left) => new BoolPoly<T> { Value = ~left.Value };
+        public bool IsZero()
+            => value.IsZero();
+
+        private int TrailingZeroCount()
+        {
+            for(int i = 0; i < T.NumWords; i++)
+            {
+                var word = value.GetWord(i);
+                var tzcnt = BitOperations.TrailingZeroCount(word);
+                if (tzcnt != 0)
+                {
+                    return (64 * i) + tzcnt;
+                }
+            }
+
+            return T.NumBits;
+        }
+
+        private int LeadingZeroCount()
+        {
+            for (int i = T.NumWords - 1; i >= 0; i--)
+            {
+                var word = value.GetWord(i);
+                var lzcnt = BitOperations.LeadingZeroCount(word);
+                if (lzcnt != 0)
+                {
+                    return (64 * i) + lzcnt;
+                }
+            }
+
+            return T.NumBits;
+        }
+
+        private IEnumerable<Monomial<T>> GetMonomials()
+        {
+            for(int wordIdx = 0;  wordIdx < T.NumWords; wordIdx++)
+            {
+                var word = value.GetWord(wordIdx);
+                for(int i = 0; i < T.NumBits; i++)
+                {
+                    var bit = 1 & (word >> i);
+                    yield return new Monomial<T>((int)bit);
+                }
+            }
+        }
+
+        public static BoolPoly<T> operator +(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { value = left.value ^ right.value };
+        public static BoolPoly<T> operator *(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { value = left.value & right.value };
+        public static BoolPoly<T> operator &(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { value = left.value & right.value };
+        public static BoolPoly<T> operator |(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { value = left.value | right.value };
+        public static BoolPoly<T> operator ^(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { value = left.value ^ right.value };
+        public static BoolPoly<T> operator ~(BoolPoly<T> left) => new BoolPoly<T> { value = ~left.value };
 
     }
 
