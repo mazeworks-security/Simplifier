34d80f0ec5805324fe0584e9fe5cf87d9e57409c
Colton1skees <Colton1skees@gmail.com>
2025-12-30 07:15:30 -0500

    more generic hacking

--- Mba.Simplifier/FastGb/BoolPoly.cs
+++ Mba.Simplifier/FastGb/BoolPoly.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Mba.Simplifier.FastGb
 {
-    public interface IVector<T, TSelf> : IAdditionOperators<TSelf, TSelf, TSelf> where TSelf: IVector<T, TSelf>
+    public interface IVector<T, TSelf> : IBitwiseOperators<TSelf, TSelf, TSelf> where TSelf: IVector<T, TSelf>
     {
         static abstract int NumVars { get; }
 
@@ -17,7 +17,7 @@ namespace Mba.Simplifier.FastGb
         static abstract int NumWords { get; }
     }
 
-    public struct U4 : IVector<Vector64<ulong>>
+    public struct U4 : IVector<Vector64<ulong>, U4>
     {
         public Vector64<ulong> Value;
 
@@ -27,11 +27,16 @@ namespace Mba.Simplifier.FastGb
 
         public static int NumWords => NumBits <= 64 ? 1 : (NumBits >> 6);
 
+        public static U4 operator +(U4 left, U4 right)
+        {
+            throw new NotImplementedException();
+        }
+
         //public static U4 operator +(U4 left, U4 right) => new U4 { Value = left.Value + right.Value };
     }
 
 
-    public struct U64 : IVector<Vector64<ulong>>
+    public struct U64 : IVector<Vector64<ulong>, U64>
     {
         public Vector64<ulong> Value;
 
@@ -44,7 +49,7 @@ namespace Mba.Simplifier.FastGb
         //public static U64 operator +(U64 left, U64 right) => new U64 { Value = left.Value + right.Value };
     }
 
-    public struct U256 : IVector<Vector256<ulong>>
+    public struct U256 : IVector<Vector256<ulong>, U256>
     {
         public Vector256<ulong> Value;
 
@@ -56,7 +61,7 @@ namespace Mba.Simplifier.FastGb
 
     }
 
-    public struct Monomial<T> where T : IVector<ulong>
+    public struct Monomial<T> where T : IVector<ulong, T>
     {
         public static int NumVars => T.NumVars;
 
@@ -65,9 +70,9 @@ namespace Mba.Simplifier.FastGb
 
 
     // Dense truth table in algebraic normal form
-    public class BoolPoly<T> where T : IVector<ulong>, IBitwiseOperators<T, T, T>
+    public class BoolPoly<T> where T : IVector<ulong, T>, IBitwiseOperators<T, T, T>
     {
-        public T Value;
+        public IVector<ulong, T> Value;
 
         public BoolPoly()
         {
@@ -81,7 +86,7 @@ namespace Mba.Simplifier.FastGb
 
         private void Tzcnt()
         {
-            value.
+          
         }
 
         public static BoolPoly<T> operator +(BoolPoly<T> left, BoolPoly<T> right) => new BoolPoly<T> { Value = left.Value ^ right.Value };
