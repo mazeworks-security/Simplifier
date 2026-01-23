4ec8ed8cbe9a5ff436cf5ca4d6087ab00a417561
Colton1skees <Colton1skees@gmail.com>
2026-01-23 14:25:16 -0500

    Start verilog parser

---
+++ Mba.Simplifier/Synthesis/VerilogParser.cs
@@ -0,0 +1,17 @@
+﻿using Mba.Simplifier.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mba.Simplifier.Synthesis
+{
+    public static class VerilogParser
+    {
+        public static void Parse(AstCtx ctx, string text)
+        {
+
+        }
+    }
+}
