56c54f7d2e5ec21ba8a77b9bc979ca9334cb0a04
Colton1skees <Colton1skees@gmail.com>
2026-03-03 11:46:35 -0500

    archive changes

--- Mba.Simplifier/Verification/GroebnerVerifier.cs
+++ Mba.Simplifier/Verification/GroebnerVerifier.cs
@@ -257,6 +257,36 @@ namespace Mba.Simplifier.Verification
             obfuscated = RustAstParser.Parse(ctx, "2*x + 2*y + 3*x + 3*y", w);
             deob = RustAstParser.Parse(ctx, "5*x + 5*y", w);
 
+            obfuscated = RustAstParser.Parse(ctx, "25*x + 25*y + 27*x + 27*y", w);
+            deob = RustAstParser.Parse(ctx, "52*x + 52*y", w);
+
+
+
+            obfuscated = RustAstParser.Parse(ctx, "2*x + 2*y + 3*x + 3*y", w);
+            deob = RustAstParser.Parse(ctx, "5*x + 5*y", w);
+
+
+            obfuscated = RustAstParser.Parse(ctx, "25*x + 25*y + 27*x + 27*y", w);
+            deob = RustAstParser.Parse(ctx, "52*x + 52*y", w);
+
+            obfuscated = RustAstParser.Parse(ctx, "2*x + 2*y + 3*x + 3*y", w);
+            deob = RustAstParser.Parse(ctx, "5*x + 5*y", w);
+
+            // negation is broken because of const offfsets
+            /*
+            obfuscated = RustAstParser.Parse(ctx, "(2*(a|((a^-1) + 1)))", w);
+            deob = RustAstParser.Parse(ctx, "(a^(((a^-1) + 1)))", w);
+            obfuscated = RustAstParser.Parse(ctx, "a^b", w);
+            deob = RustAstParser.Parse(ctx, "(a&~b)|(~a&b)", w);
+            obfuscated = RustAstParser.Parse(ctx, "~a + a + 1", w);
+            obfuscated = RustAstParser.Parse(ctx, "(a+1) - 1", w);
+            deob = RustAstParser.Parse(ctx, "a", w);
+            obfuscated = RustAstParser.Parse(ctx, "(a+1) - 1", w);
+            deob = RustAstParser.Parse(ctx, "a", w);
+            */
+
+
+
             var cache = new Dictionary<AstIdx, AstIdx>();
 
 
@@ -913,6 +943,20 @@ namespace Mba.Simplifier.Verification
 
         public void Run()
         {
+            //var xmg = XmgParser.Parse(@"C:\Users\colton\source\repos\Simplifier\Mba.Simplifier\Verification\xmg_output.txt");
+
+            //Debugger.Break();
+
+            RunReal();
+        }
+
+        public void RunAll()
+        {
+
+        }
+
+        public void RunReal()
+        {
             var throwaway = new List<(int, uint, Poly)>();
             uint totalOrder = 0;
             var firstSeen = new Dictionary<SymVar, uint>();
@@ -959,14 +1003,14 @@ namespace Mba.Simplifier.Verification
                 counts.Add(throwaway.Count);
                 SimplifyMany(ideal, trivialFacts);
 
-                /*
+                
                 var allLex = all.Select(x => x.Clone()).ToList();
                 SimplifyMany(all, trivialFacts);
                 allLex = ReduceLexGroebnerBasis(allLex, new());
                 Console.WriteLine("All lex: ");
                 foreach (var p in allLex)
                     Console.WriteLine($"    {p}");
-                */
+                
 
                 // Compute a reduced lexicographic groebner basis
                 var lexGb = ideal.Select(x => x.Clone()).ToList();
@@ -1059,9 +1103,9 @@ namespace Mba.Simplifier.Verification
 
                 // Problem: We're simplifying
                 // Using a lex reduced gb should make things much faster..
-                var nfacts = GetNonlinearFacts(ideal.Select(x => x.Clone()).ToList(), new(), false);
-                //List<Poly> nfacts = new();
-                //nfacts.AddRange(bar);
+                //var nfacts = GetNonlinearFacts(ideal.Select(x => x.Clone()).ToList(), new(), false);
+                List<Poly> nfacts = new();
+                //nfacts.AddRange(bar); 
                 SimplifyIdeal(nfacts, trivialFacts);
                 nonlinearFactLists.Add(nfacts);
 
@@ -1106,8 +1150,10 @@ namespace Mba.Simplifier.Verification
 
                 var r0 = results[0][0];
                 var r1 = results[1][0];
+
                 SimplifyViaMapping(r0, trivialFacts);
                 SimplifyViaMapping(r1, trivialFacts);
+
 
 
                 Console.WriteLine($"Result variables:\n    {r0}\n    {r1}\n");
@@ -1116,12 +1162,20 @@ namespace Mba.Simplifier.Verification
 
                 Console.WriteLine("Reductions: ");
 
+                var c0 = carryIdentifiers[deob].Item2.Last().cout;
+                var c1 = carryIdentifiers[obfuscated].Item2.Last().cout;
+                SimplifyViaMapping(c0, trivialFacts);
+                SimplifyViaMapping(c1, trivialFacts);
+                Console.WriteLine($"Carry variables: {c0}, {c1}");
+
+
+
                 var rDiff = LexReduce(diff, ideal);
+                var carryDiff = LexReduce(c1 - c0, ideal);
                 if (rDiff.Coeffs.Count != 0)
                 {
-                    Console.WriteLine(rDiff);
                     var temp = rDiff;
-
+                    //continue;
                     //continue;
 
 
@@ -1158,7 +1212,36 @@ namespace Mba.Simplifier.Verification
                     Console.WriteLine($"REDUCED to: {reduc}\n\n");
                     // Debugger.Break();
 
+                    Console.WriteLine($"carry diff: {carryDiff}\n\n");
+                    Console.WriteLine(rDiff);
+                    var sd = ReduceBySyzergies(carryDiff, rcache, definitions);
+                    Console.WriteLine($"Syzergy carry difference: {sd}");
 
+                    sd = LexReduce(sd, ideal);
+
+                    //var idealMember = (c0 - c1) - sd;
+
+                    //var idealMember = sd +
+
+                    //var bar0 = LexReduce(c0, ideal);
+                    //var bar1 = LexReduce(c1, ideal);
+
+                    // a-b == whatever
+                    // 
+
+                    var lemma = (c1 - c0) - sd;
+
+                    var first = ideals[ideals.Count - 1].SingleOrDefault(x => x.Lm == c1.Lm);
+
+
+                    //first.Coeffs.Clear();
+                    //first.Coeffs = lemma.Clone().Coeffs;
+
+                    //var ignored = -1L * (sd - c0); ;
+
+                    //trivialFacts[c1.Lm] = -1L * (sd - c0);
+
+                    Console.WriteLine("");
                 }
 
                 else
@@ -1422,10 +1505,15 @@ namespace Mba.Simplifier.Verification
 
             }
 
-
+       
             List<Poly> reducIdeal = new();
             Dictionary<Poly, Poly> rcache = new();
             var sorted = groups.OrderByDescending(x => x.Value.Lm).ToList();
+            // TODO: Sometimes there is an instance of c1*x + x
+            //if (sorted.Any(x => x.Value.IsEq(remainder)))
+            //    Debugger.Break();
+            //sorted.Clear();
+            //sorted.Add(new(Poly.Constant(1).Lm, rDiff));
             foreach (var (_, best) in sorted)
             {
                 foreach (var p in groups.Values)
@@ -1534,55 +1622,6 @@ namespace Mba.Simplifier.Verification
                 }
             }
             return result;
-        }
-
-        // This is not gonna work.. exponential time again.
-        private void GeneralizeCarriedRelationships(Poly rDiff, List<List<Poly>> ideals, Dictionary<Monomial, Poly> trivialFacts, List<Poly> nonlinearFacts, Dictionary<Poly, Poly> cache)
-        {
-            var targets = rDiff.Coeffs.Keys.SelectMany(x => x.SortedVars).Where(x => x.Kind != SymKind.Input).Distinct().ToList();
-            var targetIdx = targets.First().BitIndex;
-
-            var ideal = ideals[targetIdx];
-
-            // Compute a reduced lexicographic groebner basis
-            var lexGb = ideal.Select(x => x.Clone()).ToList();
-            lexGb = ReduceLexGroebnerBasis(lexGb, cache);
-
-
-            // Use this basis to learn new facts.
-            LearnTrivialFacts(lexGb, trivialFacts);
-
-            // Update both ideals
-            SimplifyIdeal(ideal, trivialFacts);
-            SimplifyIdeal(lexGb, trivialFacts);
-
-            //lexGb.RemoveAll(x => !targets.Contains(x.Lm.SortedVars.Single()));
-            lexGb.RemoveAll(x => x.Lm.SortedVars.Length > 1 || !targets.Contains(x.Lm.SortedVars.Single()));
-
-            var rewrites = new Dictionary<Monomial, Poly>();
-            Gauss(lexGb, rewrites);
-
-            foreach (var (monomial, rhs) in rewrites)
-            {
-                //if (rhs.Coeffs.Keys.Any(x => x.SortedVars.Count > 1 || x.SortedVars.Single().BitIndex != targetIdx || x.SortedVars.Single().Kind == SymKind.Input))
-                //     continue;
-                //if (rhs.Coeffs.Keys.Any(x => x.SortedVars.Count > 1 || x.SortedVars.Single().Kind == SymKind.Input))
-                //    continue;
-
-
-                nonlinearFacts.Add(monomial - rhs);
-                ideal.Remove(ideal.Single(x => x.Lm == monomial));
-                ideal.Add(nonlinearFacts.Last());
-            }
-
-
-            var withNonlinear = ideal.ToList();
-            withNonlinear.AddRange(nonlinearFacts);
-
-            var other = LexReduce(rDiff, withNonlinear);
-
-            Debugger.Break();
-
         }
 
         public List<Poly> LinearizeGb(List<Poly> ideal)
---
+++ Mba.Simplifier/Verification/Xmg.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Mba.Simplifier.Verification
+{
+    public enum XmgNodeType { PI, MAJ, XOR3 }
+
+    public struct XmgSignal
+    {
+        public int NodeIndex;
+        public bool IsComplemented;
+
+        public override string ToString() => (IsComplemented ? "-" : "") + NodeIndex;
+    }
+
+    public class XmgNode
+    {
+        public int Id;
+        public XmgNodeType Type;
+        public XmgSignal[] Children;
+
+        public XmgNode(int id, XmgNodeType type, XmgSignal[] children = null)
+        {
+            Id = id;
+            Type = type;
+            Children = children ?? Array.Empty<XmgSignal>();
+        }
+    }
+
+    public class XmgNetwork
+    {
+        public Dictionary<int, XmgNode> Nodes = new Dictionary<int, XmgNode>();
+        public List<int> PIs = new List<int>();
+        public List<XmgSignal> POs = new List<XmgSignal>();
+
+        public void AddPI(int id)
+        {
+            Nodes[id] = new XmgNode(id, XmgNodeType.PI);
+            PIs.Add(id);
+        }
+
+        public void AddGate(int id, string typeStr, XmgSignal[] children)
+        {
+            var type = typeStr == "MAJ" ? XmgNodeType.MAJ : XmgNodeType.XOR3;
+            Nodes[id] = new XmgNode(id, type, children);
+        }
+
+        public void AddPO(XmgSignal signal)
+        {
+            POs.Add(signal);
+        }
+    }
+
+    public class XmgParser
+    {
+        private static readonly Regex PiRegex = new Regex(@"INPUT\((\d+)\)");
+        private static readonly Regex PoRegex = new Regex(@"OUTPUT\(\d+\) = (-?\d+)");
+        private static readonly Regex GateRegex = new Regex(@"(\d+) = (MAJ|XOR3)\((.+)\)");
+
+        public static XmgNetwork Parse(string filePath)
+        {
+            var network = new XmgNetwork();
+            foreach (var line in File.ReadLines(filePath))
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var piMatch = PiRegex.Match(line);
+                if (piMatch.Success)
+                {
+                    network.AddPI(int.Parse(piMatch.Groups[1].Value));
+                    continue;
+                }
+
+                var poMatch = PoRegex.Match(line);
+                if (poMatch.Success)
+                {
+                    network.AddPO(ParseSignal(poMatch.Groups[1].Value));
+                    continue;
+                }
+
+                var gateMatch = GateRegex.Match(line);
+                if (gateMatch.Success)
+                {
+                    int id = int.Parse(gateMatch.Groups[1].Value);
+                    string type = gateMatch.Groups[2].Value;
+                    var kids = gateMatch.Groups[3].Value
+                        .Split(',')
+                        .Select(s => ParseSignal(s.Trim()))
+                        .ToArray();
+                    network.AddGate(id, type, kids);
+                }
+            }
+            return network;
+        }
+
+        private static XmgSignal ParseSignal(string s)
+        {
+            bool combined = s.StartsWith("-");
+            int id = int.Parse(combined ? s.Substring(1) : s);
+            return new XmgSignal { NodeIndex = id, IsComplemented = combined };
+        }
+    }
+}
